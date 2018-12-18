f05423642e36d1ec2e6ee1f3fa5fe12018619a38
孟凡 <mengfan@MacBook.local>
2018-12-18 22:22:45 +0800

    little process

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:post_id>/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import Post
@@ -8,3 +8,8 @@ def index(request):
     post_list = Post.objects.order_by('-modified_time')[:]
     context = {'post_list': post_list}
     return render(request, 'blog/index.html', context)
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/detail.html', {'post': post})
